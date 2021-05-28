c6d9b9d746e75d6ecd8142cc0de18c191d487ea9
Fiorenzo <fiorenzo.parascandolo@gmail.com>
2021-05-28 19:02:20 +0200

    pre-processing: added method to add period asset return

--- src/data_utils/preprocessing_utils.py
+++ src/data_utils/preprocessing_utils.py
@@ -1,5 +1,13 @@
 import pandas as pd
 
+MAX_WEEK_DAY = 4
+MAX_MONTH_DAY = 31
+MAX_MONTH = 12
+MAX_HOUR = 23
+MAX_MINUTE = 55
+STARTING_TRADE_HOUR = 9 / 23
+FINAL_TRADE_HOUR = 16 / 23
+
 
 def add_features_on_time(dataframe: pd.DataFrame) -> pd.DataFrame:
     """
@@ -8,20 +16,12 @@ def add_features_on_time(dataframe: pd.DataFrame) -> pd.DataFrame:
     - MonthDay: [1 / 31, ..., 1]
     - Month: [1/12, ..., 1]
     - Hour: [0, 1/23, ..., 1]
-    - Minute: [0, 1/59, ..., 1]
+    - Minute: [0, 5/55 ..., 1]
     - TradeHour: 1 if the market is open in that time else 0
 
     :param dataframe: dataframe to be processed.
     :return: processed dataframe.
     """
-
-    MAX_WEEK_DAY = 4
-    MAX_MONTH_DAY = 31
-    MAX_MONTH = 12
-    MAX_HOUR = 23
-    MAX_MINUTE = 59
-    STARTING_TRADE_HOUR = 9 / 23
-    FINAL_TRADE_HOUR = 16 / 23
 
     dataframe['WeekDay'] = dataframe['DateTime'].apply(lambda x: x.weekday() / MAX_WEEK_DAY)
     dataframe['MonthDay'] = dataframe['DateTime'].apply(lambda x: x.day / MAX_MONTH_DAY)
@@ -32,3 +32,27 @@ def add_features_on_time(dataframe: pd.DataFrame) -> pd.DataFrame:
 
     return dataframe
 
+
+def add_period_return(dataframe: pd.DataFrame, period: int = 1) -> pd.DataFrame:
+    """
+    Add the column 'Return' to the input dataframe. It represents.
+    Most financial studies involve returns, instead of prices, of assets.
+    There are two main advantages of using returns.
+    First, for average investors, return of an asset is a complete and scale-free summary of the investment opportunity.
+    Second, return series are easier to handle than price series because the former have more attractive statistical
+    properties.
+
+    :param dataframe: dataframe to be processed.
+    :param period: Period return
+    :return: processed dataframe.
+    """
+
+    if 'Return' not in dataframe.columns:
+        dataframe['Return'] = 1
+    # Base case
+    if period == 0:
+        return dataframe
+    dataframe['Return'] = \
+        dataframe['Return'] * dataframe['Close'].shift(periods=period - 1) / dataframe['Close'].shift(periods=period)
+    # Recursive call
+    return add_period_return(dataframe=dataframe, period=period - 1)
